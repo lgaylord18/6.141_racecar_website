28f7c4d16d8589f6935dd3bfd9ccb70b35a6d97c
Elizabeth Gaylord <Liza@dhcp-18-111-60-5.dyn.mit.edu>
2017-04-13 11:42:39 -0400

    first push

---
+++ README.md
@@ -0,0 +1,4 @@
+<h1>This is the code for the website for Green Machine <small>(team 12)</small></h1>
+
+If you would like to make changes to text in the website please go into the .html page of that page (ie
+index.html is home lab2.html is the lab 2 page) and modify the text.
---
+++ js/videoRun.js
@@ -0,0 +1,101 @@
+var figure0 = $(".video0");
+
+[].forEach.call(figure0, function (item) {
+        item.addEventListener('mouseover', hoverVideo0, false);
+        item.addEventListener('mouseout', hideVideo0, false);
+});
+
+function hoverVideo0(e) {  
+        $('.thevideo0')[0].play(); 
+}
+
+function hideVideo0(e) {
+        $('.thevideo0')[0].pause(); 
+}
+
+
+// The second Video
+var figure1 = $(".video1");
+
+[].forEach.call(figure1, function (item) {
+        item.addEventListener('mouseover', hoverVideo1, false);
+        item.addEventListener('mouseout', hideVideo1, false);
+});
+
+function hoverVideo1(e) {  
+        $('.thevideo1')[0].play(); 
+}
+
+function hideVideo1(e) {
+        $('.thevideo1')[0].pause(); 
+}
+
+
+// The third Video
+var figure2 = $(".video2");
+
+[].forEach.call(figure2, function (item) {
+        item.addEventListener('mouseover', hoverVideo2, false);
+        item.addEventListener('mouseout', hideVideo2, false);
+});
+
+function hoverVideo2(e) {  
+        $('.thevideo2')[0].play(); 
+}
+
+function hideVideo2(e) {
+        $('.thevideo2')[0].pause(); 
+}
+
+
+
+// The fourth Video
+var figure3 = $(".video3");
+
+[].forEach.call(figure3, function (item) {
+        item.addEventListener('mouseover', hoverVideo3, false);
+        item.addEventListener('mouseout', hideVideo3, false);
+});
+
+function hoverVideo3(e) {  
+        $('.thevideo3')[0].play(); 
+}
+
+function hideVideo3(e) {
+        $('.thevideo3')[0].pause(); 
+}
+
+
+
+// The fifth Video
+var figure4 = $(".video4");
+
+[].forEach.call(figure4, function (item) {
+        item.addEventListener('mouseover', hoverVideo4, false);
+        item.addEventListener('mouseout', hideVideo4, false);
+});
+
+function hoverVideo4(e) {  
+        $('.thevideo4')[0].play(); 
+}
+
+function hideVideo4(e) {
+        $('.thevideo4')[0].pause(); 
+}
+
+
+// The sixth Video
+var figure5 = $(".video5");
+
+[].forEach.call(figure5, function (item) {
+        item.addEventListener('mouseover', hoverVideo5, false);
+        item.addEventListener('mouseout', hideVideo5, false);
+});
+
+function hoverVideo5(e) {  
+        $('.thevideo5')[0].play(); 
+}
+
+function hideVideo5(e) {
+        $('.thevideo5')[0].pause(); 
+}
